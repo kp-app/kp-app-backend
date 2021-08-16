101fd9aa283bbf0f79a61fcf361c3606e50f1371
goldcvt <thegoldcat5@gmail.com>
2021-08-16 14:31:08 +0300

    Create service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,32 @@
+import { ReadProductDTO } from './dto/read-product.dto';
+import { CreateProductDTO } from './dto/create-product.dto';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pricing, Product, ProductDocument } from './schemas/product.schema';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(
+        @InjectModel(Product.name)
+        private readonly productModel: Model<ProductDocument>,
+        //@InjectModel(Pricing.name) private readonly pricingModel: Model<PricingDoc>
+    ) {}
+
+    async create(createProductDto: CreateProductDTO): Promise<Product> {
+        const createdProduct = new this.productModel(createProductDto)
+        return createdProduct.save()
+    }
+
+    // TODO filter by category
+    // use aggregations or just add string with name of category/subcategory, fuck normalization
+    async findAllByCategory(findProductDto: ReadProductDTO): Promise<Product[]> {
+        return this.productModel.find(
+            {
+                categoryName: findProductDto.categoryName,
+                subcategoryName: findProductDto.subcategoryName,
+            }, 
+            null
+        ).exec()
+    }
+}
