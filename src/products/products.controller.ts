bfe9d8c76fccc503947def3d9b8de3cd6d852fbe
goldcvt <thegoldcat5@gmail.com>
2021-08-16 11:25:56 +0300

    put products service, controller into module

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { ProductsService } from './products.service'
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Delete,
+    Param,
+    Put,
+} from '@nestjs/common'
+import { CreateProductDTO } from './dto/create-product.dto'
 
 @Controller('products')
 export class ProductsController {
-    
-    getAll (category: string): string {
-        return category
+    constructor(private readonly productsService: ProductsService) {
+        this.productsService = productsService
     }
 
+    @Get()
+    getAll(): string {
+        return 'chuch'
+    }
+
+    @Get(':id')
+    getByCategory(@Param('id') id: string): string {
+        return `robot of category with id of ${id}`
+    }
+
+    @Post()
+    create(@Body() createProductDto: CreateProductDTO): string {
+        return `Title of the product is ${createProductDto.fullName}`
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') id: string): string {
+        return id
+    }
+
+    // use updateProductDto
+    @Put(':id')
+    updateProductInfo(@Body() createProductDto: CreateProductDTO, @Param('id') id: string): string {
+        return "hey, an update!"
+    }
 }
