f76a878a29926e3a8ff3b0922818f8aaf8259be6
goldcvt <thegoldcat5@gmail.com>
2021-08-16 12:48:35 +0300

    inject new modules in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SubcategoriesModule } from './subcategories/subcategories.module';
 
 const pass: string = process.env.MONGO_PASS
 const user: string = process.env.MONGO_USER
 const dbname: string = process.env.MONGO_DBNAME
 
 @Module({
-    imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://${user}:${pass}@kp-app.ke9ej.mongodb.net/${dbname}?retryWrites=true&w=majority`)],
+    imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://${user}:${pass}@kp-app.ke9ej.mongodb.net/${dbname}?retryWrites=true&w=majority`), CategoriesModule, SubcategoriesModule],
     controllers: [AppController],
     providers: [AppService],
 })
