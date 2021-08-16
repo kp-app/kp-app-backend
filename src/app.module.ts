171917024aa676c48897f95e98ff4af8d0cbab2b
goldcvt <thegoldcat5@gmail.com>
2021-08-16 11:34:16 +0300

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
+const pass: string = process.env.MONGO_PASS
+const user: string = process.env.MONGO_USER
+const dbname: string = process.env.MONGO_DBNAME
+
 @Module({
-    imports: [ProductsModule, MongooseModule.forRoot('')],
+    imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://${user}:${pass}@kp-app.ke9ej.mongodb.net/${dbname}?retryWrites=true&w=majority`)],
     controllers: [AppController],
     providers: [AppService],
 })
