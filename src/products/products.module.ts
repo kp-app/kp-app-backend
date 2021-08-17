be47c9e490249613d3df71e3ed5a31d695ca07cf
goldcvt <thegoldcat5@gmail.com>
2021-08-17 15:02:14 +0300

    Inject linked modules, services and models

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,3 +1,4 @@
+import { SubcategoriesModule } from './../subcategories/subcategories.module';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Module } from '@nestjs/common';
@@ -8,10 +9,24 @@ import {
     Product,
     ProductSchema,
 } from './schemas/product.schema';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { CategoriesService } from 'src/categories/categories.service';
+import { Category, CategorySchema } from 'src/categories/schemas/category.schema';
+import { Subcategory, SubcategorySchema } from 'src/subcategories/schemas/subcategory.schema';
+import { SubcategoriesService } from 'src/subcategories/subcategories.service';
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}, {name: Pricing.name, schema: PricingSchema }])],
+    imports: [MongooseModule.forFeature(
+        [
+            {name: Product.name, schema: ProductSchema}, 
+            {name: Pricing.name, schema: PricingSchema },
+        ]),
+        CategoriesModule,
+        MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
+        SubcategoriesModule,
+        MongooseModule.forFeature([{ name: Subcategory.name, schema: SubcategorySchema }])
+    ],
     controllers: [ProductsController],
-    providers: [ProductsService]
+    providers: [ProductsService, CategoriesService, SubcategoriesService]
 })
 export class ProductsModule {}
