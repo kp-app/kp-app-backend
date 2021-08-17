25d6a191615c41f88ee44a0894a4c33c62ab920f
goldcvt <thegoldcat5@gmail.com>
2021-08-17 17:47:49 +0300

    add Update for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,3 +1,4 @@
+import { CategoriesService } from './../categories/categories.service';
 import { ProductsService } from './products.service'
 import {
     Controller,
@@ -10,24 +11,61 @@ import {
     Query,
 } from '@nestjs/common'
 import { CreateProductDTO } from './dto/create-product.dto'
+import { SubcategoriesService } from 'src/subcategories/subcategories.service';
+import { Product, ProductDocument } from './schemas/product.schema';
+import { UpdateProductDTO } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService: ProductsService) {}
+    constructor(
+        private readonly productsService: ProductsService, 
+        private readonly categoriesService: CategoriesService,
+        private readonly subcategoriesService: SubcategoriesService
+    ) {}
 
-    @Post()
-    create(@Body() createProductDto: CreateProductDTO): void {
-        this.productsService.create(createProductDto)
+    @Post('?')
+    create(
+            @Body() createProductDto: CreateProductDTO,
+            @Query('categoryId') productCategoryId: string
+        ): void {
+        const productDoc: Promise<ProductDocument> = this.productsService.create(createProductDto)
+        //return productId
+        productDoc.then(
+            (prodDoc: ProductDocument) => {
+                let id_ = prodDoc.toObject()._id.toString()
+                this.categoriesService.addProductToCategory(productCategoryId, id_)
+                //this.subcategoriesService.addProductToSubcategory(productId, id_)
+            }
+        )
     }
 
-    @Delete(':id')
-    removeProduct(@Param('id') id: string): string {
-        return id
+    // TODO add subcategory Id to search. First step: fetch all subcats, second step: fetch all products from it
+    // should work with subcat and without subcat
+    @Get('?')
+    getProductsByCatAndSubcat(
+        @Query('categoryId') categoryId: string,
+        @Query('subcategoryId') subcategoryId?: string
+    ): Promise<Product[]> {
+        return this.categoriesService.findAllProductsByCategory(categoryId)
+    }
+
+    // TODO add subcat id in query params
+    @Delete(':id?')
+    removeProduct(
+            @Param('id') productId: string,
+            @Query('categoryId') categoryId?: string
+        ): Promise<number> {
+        const deletedCount = this.productsService.remove(productId)
+        if (categoryId) {
+            this.categoriesService.removeProductFromCategory(categoryId, productId)
+        }
+        //this.subcategoriesService.removeProductFromSubcategory(productId)
+        return deletedCount
     }
 
     // use updateProductDto
     @Put(':id')
-    updateProductInfo(@Body() createProductDto: CreateProductDTO, @Param('id') id: string): string {
-        return "hey, an update!"
+    updateProductInfo(@Body() createProductDto: UpdateProductDTO, @Param('id') id: string) {
+        this.productsService.update(id, createProductDto)
     }
 }
