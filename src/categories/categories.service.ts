d07eed493aa6279bcb89c735eb30ae2eed53bbf2
goldcvt <thegoldcat5@gmail.com>
2021-08-17 14:12:59 +0300

    fix aggregation in categories service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,45 +1,59 @@
 // import { ReadCategoryDTO } from './dto/read-category.dto';
 import { CreateCategoryDTO } from './dto/create-category.dto';
-import { Product } from 'src/products/schemas/product.schema';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Category, CategoryDocument } from './schemas/category.schema';
 
 @Injectable()
 export class CategoriesService {
-    constructor(@InjectModel(Category.name) categoryModel: Model<CategoryDocument) {}
+    constructor(@InjectModel(Category.name) private categoryModel: Model<CategoryDocument>) {}
 
-    async findAllProductsByCategory(categoryId: string): Promise<Product[]> {
-        // TODO fix aggregation
+    async findAllProductsByCategory(categoryId: string): Promise<any[]> {
+        
+        // TODO fix return
         return this.categoryModel.aggregate([
             {
-                '$lookup': {
-                    'from': 'products',
-                    'localField': 'products',
-                    'foreignField': '_id',
-                    'as': 'productsByGroup'
+                $match: {
+                    _id: Types.ObjectId(categoryId)
                 }
-            },
+            }, 
             {
-                '$project': {
-                    '_id': 0,
-                    'productsByGroup': 1
+                $project: {
+                    _id: 0, 
+                    name: 0
                 }
-            },
+            }, 
             {
-                '$unwind': {
-                    'path': '$productsByGroup',
-                    'preserveNullAndEmptyArrays': false
+                $unwind: {
+                    path: '$products', 
+                    preserveNullAndEmptyArrays: false
                 }
-            }   
-        ],
-            function(err, products): void {}
-        ).unwind().exec()
+            }, 
+            {
+                $lookup: {
+                    from: 'products', 
+                    localField: 'products', 
+                    foreignField: '_id', 
+                    as: 'product_obj'
+                }
+            }, 
+            {
+                $unwind: {
+                    path: '$product_obj', 
+                    preserveNullAndEmptyArrays: false
+                }
+            }, {
+                $project: {
+                    products: 0
+                }
+            }
+          ]
+        ).exec()
     }
 
     async findAllCategories(): Promise<Category[]> {
-        return this.categoryModel.find().exec(); 
+        return this.categoryModel.find().exec()
     }
 
     async createCategory(createCategoryDto: CreateCategoryDTO) {
