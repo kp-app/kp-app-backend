71ae11d743c327141a9f9f5cfcb00322d1dbf49b
goldcvt <thegoldcat5@gmail.com>
2021-08-18 13:57:20 +0300

    Import CategoriesService, CategoriesModule and model

--- src/subcategories/subcategories.module.ts
+++ src/subcategories/subcategories.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
-import { SubcategoriesService } from './subcategories.service';
-import { SubcategoriesController } from './subcategories.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Subcategory, SubcategorySchema } from './schemas/subcategory.schema';
+import {Module} from '@nestjs/common';
+import {SubcategoriesService} from './subcategories.service';
+import {SubcategoriesController} from './subcategories.controller';
+import {MongooseModule} from '@nestjs/mongoose';
+import {Subcategory, SubcategorySchema} from './schemas/subcategory.schema';
+import {CategoriesService} from "../categories/categories.service";
+import {CategoriesModule} from "../categories/categories.module";
+import {Category, CategorySchema} from "../categories/schemas/category.schema";
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: Subcategory.name, schema: SubcategorySchema }])],
-    providers: [SubcategoriesService],
+    imports: [MongooseModule.forFeature([{name: Subcategory.name, schema: SubcategorySchema}]),
+        CategoriesModule,
+        MongooseModule.forFeature([{name: Category.name, schema: CategorySchema}])],
+    providers: [SubcategoriesService, CategoriesService],
     controllers: [SubcategoriesController]
 })
-export class SubcategoriesModule {}
+export class SubcategoriesModule {
+}
