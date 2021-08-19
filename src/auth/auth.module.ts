b054ecbd0563aff68af35720b3acd0d2dfc9b2d5
goldcvt <thegoldcat5@gmail.com>
2021-08-19 12:24:55 +0300

    import UsersService in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {Module} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {UsersService} from "../users/users.service";
 
 @Module({
-  providers: [AuthService]
+    imports: [UsersService],
+    providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule {
+}
