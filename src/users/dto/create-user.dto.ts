40ecb8de4ad2c314d7d1eb59b3e7373974e1ae8a
goldcvt <thegoldcat5@gmail.com>
2021-08-19 12:17:20 +0300

    update create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,6 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    private username!: string
+    private email?: string
+    private password!: string
+    private isAdmin?: boolean
+}
