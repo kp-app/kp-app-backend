9ed4b7c793d2bd7c3b8381e38629624ac3bccac0
goldcvt <thegoldcat5@gmail.com>
2021-08-19 12:46:23 +0300

    Add configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,18 @@ import {CategoriesModule} from './categories/categories.module';
 import {SubcategoriesModule} from './subcategories/subcategories.module';
 import {AuthModule} from './auth/auth.module';
 import {UsersModule} from "./users/users.module";
+import {ConfigModule} from "@nestjs/config";
+import configuration from "./config/configuration";
 
 
 @Module({
-    imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://@kp-app.ke9ej.mongodb.net/kp-app?retryWrites=true&w=majority`),
+    imports: [ProductsModule,
+        MongooseModule.forRoot(`mongodb+srv://@kp-app.ke9ej.mongodb.net/kp-app?retryWrites=true&w=majority`),
         CategoriesModule,
         SubcategoriesModule,
         AuthModule,
-        UsersModule
+        UsersModule,
+        ConfigModule.forRoot({isGlobal: true, load: [configuration]})
     ],
     controllers: [AppController],
     providers: [AppService],
