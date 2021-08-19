f1ea7c8af2e4a8f9f2b4e1d1e05b9c964d591225
goldcvt <thegoldcat5@gmail.com>
2021-08-19 17:35:22 +0300

    Fix import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import {Module} from '@nestjs/common';
 import {AuthService} from './auth.service';
-import {UsersService} from "../users/users.service";
 import {JwtModule} from "@nestjs/jwt";
 import {PassportModule} from "@nestjs/passport";
 import {JwtStrategy} from "./jwt.strategy";
+import {UsersModule} from "../users/users.module";
 
 @Module({
     imports: [
-        UsersService,
+        UsersModule,
         PassportModule,
         JwtModule.register({
             secret: process.env["jwtSecret "],
