f1bd41ce703587fa22b58681c7fa371ad901c42e
goldcvt <thegoldcat5@gmail.com>
2021-08-19 18:01:09 +0300

    Add public request to enable authorization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import {Controller, Post, Request} from '@nestjs/common'
+import {AppService} from './app.service'
+import {AuthService} from "./auth/auth.service";
+import {Public} from "./CustomDecorators";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+    constructor(
+        private readonly appService: AppService,
+        private authService: AuthService
+    ) {
+    }
 
-  @Get()
-  getHello(): string {
-    return 'Hello NestJS'
-  }
+    // Finish
+    @Public()
+    @Post('auth/login')
+    async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
 }
