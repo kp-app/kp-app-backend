856e84e88d3524041c8e5d6ec4767cccfbfc27fb
goldcvt <thegoldcat5@gmail.com>
2021-08-19 17:35:43 +0300

    reformat imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,7 +5,9 @@ import {MongooseModule} from '@nestjs/mongoose';
 import {User, UserSchema} from "./schemas/users.schema";
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
+    imports: [
+        MongooseModule.forFeature([{name: User.name, schema: UserSchema}])
+    ],
     providers: [UsersService],
     controllers: [UsersController],
     exports: [UsersService]
