31dfe88ebfb62b55dd377a4a82d6cac27763643c
goldcvt <thegoldcat5@gmail.com>
2021-08-19 13:04:15 +0300

    Use configuration.ts for providing JWT Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {UsersService} from "../users/users.service";
+import {JwtService} from "@nestjs/jwt";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+        private configService: ConfigService
+    ) {
+    }
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.usersService.findOne(username)
+        if (user && user.password === pass) {
+            const {password, ...result} = user
+            return result
+        }
+        return null
+    }
+
+    async login(user: any) {
+        const token = this.configService.get<string>('config.jwtToken')
+        const payload = {username: user.username, sub: user._id}
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
+    }
+}
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,13 @@
-export default () => ({
-    database: {
-        username: process.env.MONGO_USER,
-        password: process.env.MONGO_PASS,
-        dbname: process.env.MONGO_DBNAME
-    }
-})
+import {registerAs} from "@nestjs/config";
+
+export default registerAs(
+    'config',
+    () => ({
+        database: {
+            username: process.env.MONGO_USER,
+            password: process.env.MONGO_PASS,
+            dbname: process.env.MONGO_DBNAME
+        },
+        jwtToken: process.env.jwtToken
+    })
+)
