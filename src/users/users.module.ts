dddf9741f53e17c055082461b0ca04b65ac829c8
goldcvt <thegoldcat5@gmail.com>
2021-08-19 12:11:45 +0300

    Export UsersService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import {Module} from '@nestjs/common';
+import {UsersService} from './users.service';
+import {UsersController} from './users.controller';
+import {MongooseModule} from '@nestjs/mongoose';
+import {User, UserSchema} from "./schemas/users.schema";
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+    imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
+    controllers: [UsersController],
+    providers: [UsersService],
+    exports: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule {
+}
