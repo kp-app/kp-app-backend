21cdf108cc450929499a280a6342edb1ff0b0062
goldcvt <thegoldcat5@gmail.com>
2021-08-19 12:05:25 +0300

    Import UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { CategoriesModule } from './categories/categories.module';
-import { SubcategoriesModule } from './subcategories/subcategories.module';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ProductsModule} from './products/products.module';
+import {CategoriesModule} from './categories/categories.module';
+import {SubcategoriesModule} from './subcategories/subcategories.module';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from "./users/users.module";
 
-const pass: string = process.env.MONGO_PASS
-const user: string = process.env.MONGO_USER
-const dbname: string = process.env.MONGO_DBNAME
 
 @Module({
-    imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://${user}:${pass}@kp-app.ke9ej.mongodb.net/${dbname}?retryWrites=true&w=majority`), CategoriesModule, SubcategoriesModule],
+    imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://@kp-app.ke9ej.mongodb.net/kp-app?retryWrites=true&w=majority`),
+        CategoriesModule,
+        SubcategoriesModule,
+        AuthModule,
+        UsersModule
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
