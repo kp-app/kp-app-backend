8d12237d70683a84ceb9ca56bc5da5f80c035c04
goldcvt <thegoldcat5@gmail.com>
2021-08-19 12:10:11 +0300

    Add getting of all users, getting of one user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {Injectable} from '@nestjs/common'
+import {CreateUserDto} from './dto/create-user.dto'
+import {UpdateUserDto} from './dto/update-user.dto'
+import {InjectModel} from "@nestjs/mongoose"
+import {User, UsersDocument} from "./schemas/users.schema"
+import {Model} from "mongoose"
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+    constructor(
+        @InjectModel(User.name)
+        private readonly userModel: Model<UsersDocument>
+    ) {
+    }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    async create(createUserDto: CreateUserDto): Promise<void> {
+        const createdUser: UsersDocument = new this.userModel(createUserDto)
+        let savedUser = await createdUser.save()
+        console.log(savedUser)
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    async findAll() {
+        const users = await this.userModel.find({}).exec()
+        return users
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    async findOne(username: string) {
+        const user = await this.userModel.findOne({username: username}).exec()
+        return user
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
 }
