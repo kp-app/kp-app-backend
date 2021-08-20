ec5c8efc2a86b2fb01fcd131375cc1f65e0479bf
goldcvt <thegoldcat5@gmail.com>
2021-08-20 15:34:20 +0300

    fix return of validateUser (now plain obj), implement decodeUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,16 +15,22 @@ export class AuthService {
     async validateUser(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(username)
         if (user && user.password === pass) {
-            const {password, ...result} = user
+            const {password, ...result} = user.toObject()
             return result
         }
         return null
     }
 
+    // for logout we'll just dump token from frontend state
     async login(user: any) {
-        const payload = {username: user.username, sub: user._id}
+        const payload = {username: user.username, sub: user._id, role: user.role}
         return {
             access_token: this.jwtService.sign(payload, {expiresIn: this.configService.get('expiresIn')})
         }
+    }
+
+    decodeUser(token: string) {
+        const userDecoded = this.jwtService.decode(token)
+        return userDecoded
     }
 }
