b938169e33ecc7e359acfed3f0431de28040ac19
goldcvt <thegoldcat5@gmail.com>
2021-08-20 15:47:26 +0300

    Add roles guards to users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,22 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import {UsersService} from './users.service';
 import {CreateUserDto} from './dto/create-user.dto';
 import {UpdateUserDto} from './dto/update-user.dto';
+import {Roles} from "../roles/roles.decorator";
+import {Role} from "../roles/enums/roles.enum";
 
+@Roles(Role.Admin)
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {
     }
 
+
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
     }
-
+    
     @Get()
     findAll() {
         return this.usersService.findAll();
